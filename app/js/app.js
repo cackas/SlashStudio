9024c93bda02c052310d9ec63c1a04c7c3e75edc
cackas <vannigga@yandex.ru>
2021-05-20 16:56:51 +0300

    carousel

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,37 @@
 // // Import jQuery module (npm i jquery)
-// import $ from 'jquery'
-// window.jQuery = $
-// window.$ = $
-
+import $ from 'jquery'
+window.jQuery = $
+window.$ = $
+require('~/app/libs/slick-carousel/slick/slick.js')
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
 
 document.addEventListener('DOMContentLoaded', () => {
-
 	// Custom JS
+	$('.car-1-wrap').slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear'
+	  });
+	$('.case-wrap').slick({
+		vertical: true,
+		arrows: false,
+		centerMode: true,
+		slidesToShow: 3,
+		swipe: true,
+		slidesToScroll: 1,
+		focusOnSelect: true,
+		asNavFor: '.car-1-wrap'
+	  });
 
+	  $('.arr-up').click(function(){
+		$('.case-wrap').slick('slickPrev');
+	  })
+	  
+	  $('.arr-bott').click(function(){
+		$('.case-wrap').slick('slickNext');
+	  })
+	  
 })
